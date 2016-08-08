1164b615b4265defab5425920ae151abd79ec6ba
Leo Lamprecht <mindrun@icloud.com>
2016-08-08 13:03:24 +0200

    Use xo for linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,19 @@
 const gulp = require('gulp')
 const del = require('del')
 const cache = require('gulp-cached')
-const eslint = require('gulp-eslint')
 const help = require('gulp-task-listing')
 const babel = require('gulp-babel')
 const ext = require('gulp-ext')
 
 gulp.task('help', help)
 
-gulp.task('compile', function () {
+gulp.task('compile', () => {
   return gulp.src('bin/**/*')
   .pipe(cache('bin'))
   .pipe(babel())
   .pipe(ext.crop())
-  .pipe(gulp.dest('build/bin'))
+  .pipe(gulp.dest('dist/bin'))
 })
 
-gulp.task('lint', function () {
-  return gulp.src([
-    'gulpfile.js',
-    'bin/*'
-  ])
-  .pipe(eslint())
-  .pipe(eslint.format())
-  .pipe(eslint.failAfterError())
-})
-
-gulp.task('clean', () => del(['build']))
+gulp.task('clean', () => del(['dist']))
 gulp.task('default', ['lint', 'compile'])
